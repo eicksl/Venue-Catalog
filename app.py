f871f5a10168088c1b88ef3bcb04112f4374bc2f
eicksl <spencereick@gmail.com>
2017-10-17 15:05:26 +0000

    Improve search functionality

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import jsonify, redirect
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from db_setup import Base, City, Venue
-import json, requests, httplib2
+import json, requests, httplib2, os
 
 
 app = Flask(__name__)
@@ -16,7 +16,26 @@ CLIENT_ID = 'X51LXWV4BDKANESBY1PCWEG2XDDG4FW0PTWFWOGXX0YTO5EL'
 CLIENT_SECRET = 'Q4EWJUPCQM114AESG5VKYLVLGRVZLDFW1OA3KPERTMIP2R02'
 
 
+@app.context_processor
+def override_url_for():
+    '''http://flask.pocoo.org/snippets/40'''
+    return dict(url_for=dated_url_for)
+
+
+def dated_url_for(endpoint, **values):
+    '''http://flask.pocoo.org/snippets/40'''
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+
+
 def get_coordinates(location):
+    '''Takes in a location string and uses the Google Geocode API to return
+    a string of coordinates for the location'''
     GOOGLE_API_KEY = 'AIzaSyAQCG4wcNxQHbYQ9WYstLWVb03HC_lDKeI'
     url = 'https://maps.googleapis.com/maps/api/geocode/json'
     params = {'key': GOOGLE_API_KEY, 'address': location}
@@ -27,6 +46,7 @@ def get_coordinates(location):
         longitude = data['results'][0]['geometry']['location']['lng']
         return str(latitude) + ',' + str(longitude)
     except (KeyError, IndexError):
+        flash('No results found. Please try modifying your search.')
         return None
 
 
@@ -40,12 +60,12 @@ def get_image(venue_api_id):
     }
     p_resp = requests.get(photo_url, photo_params)
     p_data = json.loads(p_resp.text)
-    if p_data['response']['photos']['items']:
+    try:
         prefix = p_data['response']['photos']['items'][0]['prefix']
         suffix = p_data['response']['photos']['items'][0]['suffix']
         image_url = prefix + '300x300' + suffix
         return image_url
-    else:
+    except KeyError:
         return None
 
 
@@ -54,8 +74,10 @@ def get_venues(query, location, offset=0):
     LIMIT = 11
     results = []
     coordinates = get_coordinates(location)
+
     if not coordinates:
         return None
+
     url = 'https://api.foursquare.com/v2/venues/explore'
     params = {
         'client_id': CLIENT_ID,
@@ -69,8 +91,13 @@ def get_venues(query, location, offset=0):
     resp = requests.get(url, params)
     data = json.loads(resp.text)
 
+    if data['meta']['code'] != 200:
+        flash('An error occurred while processing the request.')
+        return None
+
     num_of_venues = len(data['response']['groups'][0]['items'])
     if num_of_venues == 0:
+        flash('No results found. Please try modifying your search.')
         return None
 
     for index in range(num_of_venues):
@@ -80,8 +107,14 @@ def get_venues(query, location, offset=0):
         info = {}
         venue = data['response']['groups'][0]['items'][index]['venue']
         info['name'] = venue['name']
-        info['phone'] = venue['contact']['formattedPhone']
-        address = venue['location']['formattedAddress']
+        try:
+            info['phone'] = venue['contact']['formattedPhone']
+        except KeyError:
+            pass
+        try:
+            address = venue['location']['formattedAddress']
+        except KeyError:
+            pass
         info['address'] = '\n'.join(address)
         try:
             info['description'] = (data['response']['groups'][0]
@@ -98,20 +131,26 @@ def get_venues(query, location, offset=0):
 @app.route('/', methods=['GET','POST'])
 def catalog():
     if request.method == 'POST':
-        return redirect(url_for('search'), data=data,
-            query=request.form['query'], location=request.form['location'])
+        return redirect(url_for('search'), query=request.form['query'],
+               location=request.form['location'])
     else:
         return render_template('main.html')
 
 
 @app.route('/search')
 def search():
-    data = get_venues(request.args.get('query'), request.args.get('location'))
+    if request.args.get('offset') is None:
+        offset = 0
+    else:
+        offset = request.args.get('offset')
+
+    data = get_venues(request.args.get('query'), request.args.get('location'),
+           offset)
     if data is None:
-        flash('No results found. Please try modifying your search.')
         return redirect(url_for('catalog'))
 
-    return render_template('search.html', data=data,
+    #return json.dumps(offset)
+    return render_template('search.html', data=data, offset=offset,
         location=request.args.get('location'), query=request.args.get('query'))
 
 
--- templates/js/search.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-   // hello()
-});
-
-
-// var text = {{data[0]['description']}};
-function hello() {
-  $('body').append("<p class='hello'>Hello!</p>");
-}
